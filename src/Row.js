621d10fe456cee2f492a7dba3b1f0972d5841233
Fabio <fabio.cattolico@gmx.de>
2022-08-21 10:16:49 -0500

    Final Version

--- src/Row.js
+++ src/Row.js
@@ -9,7 +9,7 @@ import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl, isLargeRow }) {
+function Row({ title, fetchUrl, isLargeRow, }) {
     const [movies, setMovies] = useState([]);
     const [trailerUrl, setTrailerUrl] = useState("");
 
@@ -61,14 +61,16 @@ function Row({ title, fetchUrl, isLargeRow }) {
             <h2>{title}</h2>
 
             <div className="row__posters">
+               
                 {movies.map(movie => (
                     <img 
                         key={movie.id}
                         onClick={() => handleClick(movie)}
                         className= {`row__poster ${isLargeRow && "row__posterLarge"}`}
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
+                        
                 ))}
-             </div> 
+            </div> 
              {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} /> }
 
         </div>
